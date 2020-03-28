fae81c752fd1c09c3a5e8f56a3dd7b3953496b1a
ashoka <ashoka.devara89@gmail.com>
2020-03-28 20:20:14 +0530

    first commit...

---
+++ src/com/ashoka/git/GitTest1.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.ashoka.git;
+
+/**
+ * @author ASHOKA
+ *
+ */
+public class GitTest1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 40;
+		int b = 5;
+		a = a * b;
+		b = a / b;
+		a = a / b;
+		System.out.println("a value is " + a);
+		System.out.println("b value is " + b);
+	}
+
+}
