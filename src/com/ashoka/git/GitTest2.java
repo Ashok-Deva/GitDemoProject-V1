2c4e4672c4de6107fcc3c9d7e0f68eda15ab55bc
ashoka <ashoka.devara89@gmail.com>
2020-03-28 23:42:15 +0530

    added one more System.out.println

--- src/com/ashoka/git/GitTest2.java
+++ src/com/ashoka/git/GitTest2.java
@@ -14,6 +14,7 @@ public class GitTest2 {
 	 */
 	public static void main(String[] args) {
 		String s1="india";
+		System.out.println(s1);
 		String rev="";
 		for(int i=s1.length()-1;i>=0;i--) {
 			rev=rev+s1.charAt(i);
